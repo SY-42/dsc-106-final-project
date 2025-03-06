399dea5645cfa0d40e80dbbb3d4fc1ff991234b4
SY-42 <103295003+SY-42@users.noreply.github.com>
2025-03-05 20:47:43 -0800

    add global dataset

--- global.js
+++ global.js
@@ -1,6 +1,19 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 import { averageHeartRateData } from "./scripts/utils.js";
 
+// set the dimensions and margins of country graph
+var margin = {top: 20, right: 30, bottom: 40, left: 150},
+    countryWidth = 460 - margin.left - margin.right,
+    countryHeight = 400 - margin.top - margin.bottom;
+
+const countryParser = (row) => {
+    return {
+        name: row["Country Name"],
+        code: row['Country Code'],
+        prevalence: row['2021'],
+    }
+}
+
 async function loadData(fileName) {
   try {
     const parser = fileName.startsWith("./data/Dexcom") ? parseGlucoseData : parseFoodData;
@@ -280,6 +293,70 @@ function updatePlots() {
 }
 
 
+
+
+let currentCountry;
+
+const displayStats = (filtered_stats) => {
+
+    d3.select("#global-stats").html("")
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#global-stats")
+        .append("svg")
+        .attr("width", countryWidth + margin.left + margin.right)
+        .attr("height", countryHeight + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleLinear()
+        .domain([0, 35])
+        .range([ 0, countryWidth]);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + countryHeight + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Y axis
+    var y = d3.scaleBand()
+        .range([ 0, countryHeight ])
+        .domain(filtered_stats.map(d => d.name))
+        .padding(.1);
+
+    svg.append("g")
+        .call(d3.axisLeft(y))
+
+    //Bars
+    svg.selectAll("myRect")
+        .data(filtered_stats)
+        .enter()
+        .append("rect")
+        .attr("x", x(0) )
+        .attr("y", (d) => y(d.name))
+        .attr("width", (d) => x(d.prevalence))
+        .attr("height", y.bandwidth() )
+        .attr("fill", (d) => d.name == currentCountry ? "#0096FF" :"#69b3a2")
+        
+
+    //Text
+    svg.selectAll("myRect")
+        .data(filtered_stats)
+        .enter()
+        .append("text")
+        .attr("x", (d) => x(d.prevalence)-4 )
+        .attr("y", (d) => y(d.name)+12)
+        .attr("font-size", 'smaller')
+        .attr('text-anchor', 'end')
+        .attr("fill", "#69b3a2")
+        .text((d) => parseFloat(d.prevalence).toFixed(1))
+        .attr("fill", "#D3D3D3")
+}
+
+
 // Function to load data and create plots
 async function main(dataset = "001") {
   d3.select("#chart").selectAll("*").remove();
@@ -302,6 +379,33 @@ async function main(dataset = "001") {
   createScatterplot(data);
   createFoodPlot(glucoseData, foodData);
   updateSliderLabel();
+
+  // Retrieve the country data
+  const diabetes_stats = await d3.csv("./data/global_stats.csv", countryParser);
+  // We should filter for non-countries here
+
+  // filter the top 10
+
+  let filtered_stats = diabetes_stats
+      .filter((d) => d.prevalence != NaN)
+      .sort((a,b) => b.prevalence - a.prevalence)
+
+  displayStats(filtered_stats.slice(0, 20))
+
+  document.getElementById("country-input").addEventListener("input", function(){
+      let results = filtered_stats.filter(
+          (d) => d.name.toLowerCase().includes(this.value.toLowerCase())
+      )
+      if (results.length != 0){
+          const idx = filtered_stats.findIndex(
+              (d) => d.name == results[0].name
+          )
+          currentCountry = results[0].name
+          displayStats(filtered_stats
+              .slice(Math.max(idx-10, 0), idx >= 10 ? idx+10 : 20)
+          )
+      }
+  })
 }
 
 // Change the selected participant when selected in dropdown
